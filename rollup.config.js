f760cc6f23fe1a2731d324bc1f3de000178f6a42
uiui611 <uiui611@gmail.com>
2019-01-01 19:30:34 +0900

    To remove comments and trim lines to create bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
+import package_json from './package.json';
+const banner = `
+/*
+ * @mizu-mizu/array-matcher.js VERSION ${package_json.version}
+ * Released under the MIT License.
+ */
+`;
+function transform(code, id){
+    return code.split(/\r?\n/g)
+        .map(line=>line.trim())
+        .filter(line=>!/[\/ ]?\*/.test(line.substring(0, 2)))
+        .filter(line=>line.trim().length)
+        .join('\n');
+}
 export default {
     input: 'src/index.mjs',
     output:[
         {
             file: 'dist/array-matcher.mjs',
-            format: 'esm'
+            format: 'esm',
+            banner
         },
         {
             file: 'dist/array-matcher.js',
-            format: 'cjs'
+            format: 'cjs',
+            banner
         }
-    ]
+    ],
+    plugins:[ {transform } ]
 }
